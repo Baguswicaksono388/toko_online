95ceeef99875f949c3b13b9c8a6c0d273e5c5a23
Bagus Wicaksono <baguswicaksono388@gmail.com>
2020-12-15 08:01:06 +0700

    Dev (Bagus) Adding function tambah_barang on Model M_Barang

--- application/models/M_Barang.php
+++ application/models/M_Barang.php
@@ -5,4 +5,8 @@ class M_Barang extends CI_Model {
     public function tampil_data() {
         return $this->db->get('tb_barang');
     }
+
+    public function tambah_barang($data, $table) {
+        $this->db->insert($table, $data);
+    }
 }
