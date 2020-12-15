8f49a72bb84cbc12d03b5324c9762b59be1a4516
Bagus Wicaksono <baguswicaksono388@gmail.com>
2020-12-15 08:00:27 +0700

    Dev (Bagus) Adding function tambah_aksi on Controller Data_barang

--- application/controllers/admin/Data_barang.php
+++ application/controllers/admin/Data_barang.php
@@ -11,6 +11,36 @@ class Data_barang extends CI_Controller {
     }
 
     public function tambah_aksi() {
-        
+        $nama_brg   = $this->input->post('nama_brg');
+        $keterangan   = $this->input->post('keterangan');
+        $kategori   = $this->input->post('kategori');
+        $harga   = $this->input->post('harga');
+        $stok   = $this->input->post('stok');
+        $gambar = $_FILES['gambar']['name'];
+        if ($gambar='') {} else {
+            $config ['upload_path'] = './uploads';
+            $config ['allowed_types'] = 'jpg|jpeg|png|gif';
+
+            $this->load->library('upload', $config);
+            if(!$this->upload->do_upload('gambar')) {
+                echo "Gambar Gagal di Upload";
+            } else {
+                $gambar = $this->upload->data('file_name');
+            }
+        }
+
+
+        $data = array (
+            'nama_brg' => $nama_brg,
+            'keterangan' => $keterangan,
+            'kategori'  => $kategori,
+            'harga'     =>$harga,
+            'stok'      =>$stok,
+            'gambar'    =>$gambar
+
+        );
+
+        $this->M_Barang->tambah_barang($data, 'tb_barang');
+        redirect('../admin/Data_barang');
     }
 }
