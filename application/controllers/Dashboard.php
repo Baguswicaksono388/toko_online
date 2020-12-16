f3dfdc3808a98ea83847207c94b9e3e30a26ebcb
Bagus Wicaksono <baguswicaksono388@gmail.com>
2020-12-16 10:35:31 +0700

    Dev (Bagus)

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Dashboard extends CI_Controller {
     public function index() {
         $data['barang'] = $this->M_Barang->tampil_data()->result();
-        $this->load->view('templates/header1');
+        $this->load->view('templates/header');
         $this->load->view('templates/sidebar');
         $this->load->view('dashboard', $data);
         $this->load->view('templates/footer');
