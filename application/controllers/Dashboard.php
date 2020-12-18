25d9c5751f7ad081649f23b72dbe82ebf1dc1e21
Bagus Wicaksono <baguswicaksono388@gmail.com>
2020-12-18 15:08:38 +0700

    Dev (Bagus) Adding function tambah_ke_keranjang on Controller Dashboard

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -9,4 +9,19 @@ class Dashboard extends CI_Controller {
         $this->load->view('dashboard', $data);
         $this->load->view('templates/footer');
     }
+
+    public function tambah_ke_keranjang($id) {
+        $barang = $this->M_Barang->find($id);
+
+        //memasukan data ke keranjang
+        $data = array(
+            'id'      => $barang->id_brg,
+            'qty'     => 1,
+            'price'   => $barang->harga,
+            'name'    => $barang->nama_brg,
+        );
+    
+        $this->cart->insert($data);
+        redirect('Dashboard');
+    }
 }
