f5a1a60497b3d2b4ff634872a24c814e964af64f
Bagus Wicaksono <baguswicaksono388@gmail.com>
2020-12-19 06:52:50 +0700

    Dev (Bagus) Adding M_Invoce on autoload model

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('M_Barang');
+$autoload['model'] = array('M_Barang','M_Invoice');
